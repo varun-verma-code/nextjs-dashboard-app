db39d4169ba6a9ef6cc3f8981015144ad8bd08d3
varun-verma-code <verma.varun@gmail.com>
2024-11-06 11:10:31 -0800

    Chapter 9 - Loading Skeleton for page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,5 @@
-export default function Page() {
+export default async function Page() {
+  console.log('Delay in dashboard/invoices');
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return <div>Invoices Page</div>;
 }
