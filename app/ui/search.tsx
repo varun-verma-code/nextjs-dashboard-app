e5cd9ad2913d40e1624b2e85b3e1fb0399856b5e
varun-verma-code <verma.varun@gmail.com>
2024-11-09 06:30:34 -0800

    Chapter 11 - Add Debouncing to search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   // Returns a ReadonlyURLSearchParams. To obtain the existing search params from the URL
@@ -14,8 +15,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    // console.log(term);
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(term);
     // URLSearchParams used for advanced handling. Since useSearchParams returns a readonly object
     const params = new URLSearchParams(searchParams);
     if (term) {
@@ -24,7 +25,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
